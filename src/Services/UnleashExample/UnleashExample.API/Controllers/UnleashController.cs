165f1badad35780e02dd34f22b97b2ff46d9ad42
hexsorcerer <hexsorcerer03e9@gmail.com>
2023-02-13 20:50:37 -0500

    feat: add first working version of feature flag
    This was tricky to get just right, I think the final piece that was missing was the ProjectId in the
    configuration had to have a capital D as in Default and not default, even though it appears that way
    in some portions of the web UI. Also have to make sure you're using the right types of tokens for
    the right types of things.

--- src/Services/UnleashExample/UnleashExample.API/Controllers/UnleashController.cs
+++ src/Services/UnleashExample/UnleashExample.API/Controllers/UnleashController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging.Abstractions;
+using Unleash;
 
 namespace UnleashExample.Controllers;
 
@@ -8,14 +10,20 @@ public class UnleashController : ControllerBase
 {
     private readonly ILogger<UnleashController> _logger;
 
-    public UnleashController(ILogger<UnleashController> logger)
+    private readonly IUnleash _unleash;
+
+    public UnleashController(IUnleash unleash, ILogger<UnleashController>? logger = null)
     {
-        _logger = logger;
+        _logger = logger ?? NullLogger<UnleashController>.Instance;
+
+        _unleash = unleash ?? throw new ArgumentNullException(nameof(unleash));
     }
 
     [HttpGet(Name = "GetUnleash")]
     public ActionResult Get()
     {
-        return Ok("unleash");
+        return Ok(_unleash.IsEnabled("ExampleFeature")
+            ? "feature flag is enabled"
+            : "feature flag is disabled");
     }
 }
--- src/Services/UnleashExample/UnleashExample.API/Program.cs
+++ src/Services/UnleashExample/UnleashExample.API/Program.cs
@@ -1,6 +1,34 @@
+using Unleash;
+using Unleash.ClientFactory;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// TODO: delete me
+var settings = new UnleashSettings
+{
+    AppName = "unleash-example",
+    ProjectId = "Default",
+    UnleashApi = new Uri(@"http://unleash-server:4242/api/"),
+    CustomHttpHeaders = new Dictionary<string, string>
+    {
+        {
+            "Authorization",
+            $@"{Environment.GetEnvironmentVariable("UNLEASH_API_TOKEN") ?? string.Empty}"
+        }
+    }
+};
+
+var unleashFactory = new UnleashClientFactory();
+
+var unleash = await unleashFactory
+    .CreateClientAsync(settings, synchronousInitialization: true)
+    .ConfigureAwait(false);
+
+unleash.ConfigureEvents(cfg =>
+{
+    cfg.ImpressionEvent = evt => { Console.WriteLine($"{evt.FeatureName}: {evt.Enabled}"); };
+});
+
+builder.Services.AddSingleton(unleash);
 
 // Add services to the container.
 
